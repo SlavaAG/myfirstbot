d74e466ed1e7844dedba5ce0994754334bbf83af
Slava Grachev <slavasig@mail.ru>
2019-03-02 15:47:21 +0300

    second_version

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import settings
+import ephem
 
 
 logging.basicConfig(format = '%(asctime)s - %(levelname)s - %(message)s',
@@ -21,6 +22,34 @@ def talk_to_me(bot, update):
 	update.message.reply_text(user_text)
 
 
+def horoscope(bot, update):
+	user_planet = update.message.text.split()
+	if user_planet[1] == 'Mars':
+		planet = ephem.Mars('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Pluto':
+		planet = ephem.Pluto('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Saturn':
+		planet = ephem.Saturn('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Venus':
+		planet = ephem.Venus('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Jupiter':
+		planet = ephem.Jupiter('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Neptun':
+		planet = ephem.Neptun('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Mercury':
+		planet = ephem.Mercury('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+	elif user_planet[1] == 'Uranus':
+		planet = ephem.Uranus('2019/03/02')
+		update.message.reply_text(ephem.constellation(planet))
+
+
 def main():
 	mybot = Updater(settings.API_KEY, request_kwargs = settings.PROXY)
 
@@ -29,6 +58,7 @@ def main():
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
 	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+	dp.add_handler(CommandHandler("planet", horoscope))
 
 	mybot.start_polling()
 	mybot.idle()
